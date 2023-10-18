c1978e906ec532fa964d987f6326e7865f92059c
WhannJaii <107906957+chatwimonbis3r2@users.noreply.github.com>
2023-10-18 18:19:27 +0700

    18-10-2023

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+var colors = require("colors");
+const debug = require('debug')('app')
+const morgan = require('morgan')
+
+const app = express();
+const port = 80;
+
+app.use(morgan('combined'))
+
+app.get("/", (req, res) => {
+  res.send("Hello Express NodeJs");
+});
+
+app.listen(port, () => {
+  debug(colors.brightGreen("Listening On Port: " + port));
+});
