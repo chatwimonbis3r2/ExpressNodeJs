310372e87f06fa687c1e67e59bee805582be84fe
WhannJaii <107906957+chatwimonbis3r2@users.noreply.github.com>
2023-10-18 19:59:42 +0700

    with template

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 var colors = require("colors");
 const debug = require('debug')('app')
 const morgan = require('morgan')
+const path = require('path')
 
 const app = express();
 const port = 80;
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req, res) => {
   res.send("Hello Express NodeJs");
